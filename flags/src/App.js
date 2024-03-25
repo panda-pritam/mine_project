e95baca17cb2d72dd8c8d1a7401b7e07dac5b094
panda-pritam <mondalrumki02@gmail.com>
2024-03-26 00:58:03 +0530

    cards added

--- flags/src/App.js
+++ flags/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+// import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
 function App() {
+  let [flagList, setFlagList] = useState([]);
+  let [cardList, setCardList] = useState([]);
+  useEffect(() => {
+    try {
+      fetch("https://restcountries.com/v3.1/all")
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          console.log(res);
+          setFlagList(res);
+        })
+        .catch((err) => {
+          console.log("err-> ", err);
+        });
+    } catch (error) {}
+  }, []);
+  useEffect(() => {
+    if (flagList.length) {
+      let cardList = flagList.map((ele) => {
+        return (
+          <Grid
+            item
+            xl={12 / 7}
+            lg={5}
+            md={4}
+            sm={3}
+            key={ele.name.common}
+            className="cardbox"
+          >
+            <img
+              src={ele.flags.png}
+              alt={ele.flags.alt}
+              width="100px"
+              height="100px"
+            />
+            <h2 className="ctyName">{ele.name.common}</h2>
+          </Grid>
+        );
+      });
+      setCardList(cardList);
+    }
+  }, [flagList]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainDiv">
+      {/* <h1>country flag</h1> */}
+      <Box>
+        <Grid container spacing={2}>
+          {cardList}
+        </Grid>
+      </Box>
     </div>
   );
 }
