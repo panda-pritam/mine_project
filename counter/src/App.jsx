0898e1385c33c079ac5c34e1af29c7f6d1f0499d
panda-pritam <mondalrumki02@gmail.com>
2024-03-27 03:15:02 +0530

    counter coded

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,10 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let oncleckHandler = (e) => {
+    if (e.target.id === "inc") {
+      setCount((prev) => prev + 1);
+    } else {
+      setCount((prev) => prev - 1);
+    }
+  };
   return (
-    <div>
-      <h1>Counter</h1>
+    <div className="mainDiv">
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button type="button" id="inc" onClick={oncleckHandler}>
+        Increment
+      </button>
+      <button type="button" id="dec" onClick={oncleckHandler}>
+        Decrement
+      </button>
     </div>
   );
 }
