8e5e6f377193ea70068f06632f5f9b2225217a39
panda-pritam <mondalrumki02@gmail.com>
2024-03-26 18:46:15 +0530

    btn types added

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -34,6 +34,7 @@ function App() {
     <div className="mainDiv">
       <h1>React Calculator</h1>
       <input
+        type="text"
         value={equation}
         className="result"
         onChange={(e) => {
@@ -42,52 +43,136 @@ function App() {
       />
       {result ? <div className="output">{result}</div> : ""}
       <form onSubmit={onSubmitHandler} className="btnDiv">
-        <button id="7" value="7" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="7"
+          value="7"
+          onClick={onClickHandler}
+          className="btn"
+        >
           7
         </button>
-        <button id="8" value="8" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="8"
+          value="8"
+          onClick={onClickHandler}
+          className="btn"
+        >
           8
         </button>
-        <button id="9" value="9" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="9"
+          value="9"
+          onClick={onClickHandler}
+          className="btn"
+        >
           9
         </button>
-        <button id="+" value="+" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="+"
+          value="+"
+          onClick={onClickHandler}
+          className="btn"
+        >
           +
         </button>
-        <button id="4" value="4" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="4"
+          value="4"
+          onClick={onClickHandler}
+          className="btn"
+        >
           4
         </button>
-        <button id="5" value="5" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="5"
+          value="5"
+          onClick={onClickHandler}
+          className="btn"
+        >
           5
         </button>
-        <button id="6" value="6" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="6"
+          value="6"
+          onClick={onClickHandler}
+          className="btn"
+        >
           6
         </button>
-        <button id="-" value="-" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="-"
+          value="-"
+          onClick={onClickHandler}
+          className="btn"
+        >
           -
         </button>
-        <button id="1" value="1" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="1"
+          value="1"
+          onClick={onClickHandler}
+          className="btn"
+        >
           1
         </button>
-        <button id="2" value="2" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="2"
+          value="2"
+          onClick={onClickHandler}
+          className="btn"
+        >
           2
         </button>
-        <button id="3" value="3" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="3"
+          value="3"
+          onClick={onClickHandler}
+          className="btn"
+        >
           3
         </button>
-        <button id="*" value="*" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="*"
+          value="*"
+          onClick={onClickHandler}
+          className="btn"
+        >
           *
         </button>
-        <button type="reset" className="btn" onClick={clearText}>
+        <button type="button" className="btn" onClick={clearText}>
           C
         </button>
-        <button id="0" value="0" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="0"
+          value="0"
+          onClick={onClickHandler}
+          className="btn"
+        >
           0
         </button>
         <button type="submit" className="btn" onSubmit={onSubmitHandler}>
           =
         </button>
-        <button id="/" value="/" onClick={onClickHandler} className="btn">
+        <button
+          type="button"
+          id="/"
+          value="/"
+          onClick={onClickHandler}
+          className="btn"
+        >
           /
         </button>
       </form>
